e191a2ad27bc7444edd8fd54fa50c4bfe2f101aa
Maciej <82758495+Kanapkador@users.noreply.github.com>
2022-09-14 21:48:05 +0200

    dsdasdas
    wdasda

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8008 });
-wss.on('connection', (ws) => {
-    const id = uuidv4();
-    const color = Math.floor(Math.random() * 360);
-    const metadata = { id, color };
-    clients.set(ws, metadata);
-});
-const clients = new Map();
+const path = require("path");
 
-app.on('/', (req, res) => {
-    console.log('Server received request');
-})
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 7777 });
+
+wss.on("connection", (ws) => {
+  console.log("content");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(8008, () => {
-    console.log(`Server listening to port 8008`);
-})
+  console.log(`Server listening to port 8008`);
+});
