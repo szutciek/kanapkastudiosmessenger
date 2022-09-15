0653e3c441065397bc08cbaf9d76bdea959b538c
jqsx <101830653+jqsx@users.noreply.github.com>
2022-09-15 14:37:04 +0200

    4 big guys

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 7777 });
 
 wss.on("connection", (ws) => {
-  console.log("content");
+    console.log("content");
+    ws.send('jfoisjfiodjfdosi')
+    ws.on('message', (ws) => {
+        console.log(ws)
+    });
+    ws.emit(JSON.stringify({ data: "suck cock" }))
 });
 
+
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/test", (req, res) => {
+    res.send({ test: "test" })
 });
 
 app.listen(8008, () => {
-  console.log(`Server listening to port 8008`);
+    console.log(`Server listening to port 8008`);
 });
