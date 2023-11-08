000dbe59afa4a6f61a0734f0f1e32aa0a91d5ba2
jqsx <101830653+jqsx@users.noreply.github.com>
2023-11-08 17:14:19 +0100

    a

---
+++ backend/Messenger.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+function createServer() {
+    const app = express();
+
+    app.set('view engine', 'ejs');
+
+    app.listen(3000, () => {
+        console.log(`listening on 3000`);
+    })
+
+    return app;
+}
+
+function setGetRoute(app, path, callback) {
+    app.get(path, callback);
+}
+
+function setPostRoute(app, path, callback) {
+    app.post(path, callback);
+}
+
+module.exports = { createServer, setGetRoute, setPostRoute };
---
+++ index.js
@@ -0,0 +1,7 @@
+const Messenger = require('./backend/Messenger.js');
+
+const app = Messenger.createServer();
+
+Messenger.setGetRoute(app, '/', (req, res) => {
+    res.render('pages/index');
+})
